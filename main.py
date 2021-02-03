487fb706c5656a33dbbec161e979315a07d532d6
Grrrigori <gregoryest@gmail.com>
2021-02-03 16:40:07 +0200

    Lesson 2

---
+++ main.py
@@ -0,0 +1,94 @@
+# # name = 'Grigori'
+# # print(name)
+# #
+# # print(type(name))
+# #
+# # integer = 234
+# # print(type(integer))
+# #
+# # float = 2.4
+# # print( type(float))
+# #
+# # some_number = 5.0
+# # print(type(some_number))
+# # boolean = True
+# # print(type(boolean))
+# #
+# # none_value = None
+# # print(type(none_value))
+#
+# new_variable = '546'
+#
+# print(help(new_variable)
+#
+a = 500
+b = 200
+# print(a+b)
+# print(2+2)
+# print (a+100)
+#
+# c= a+b
+# print(c)
+#
+# print(a +20.8)
+# print(a-b)
+# print(3-1)
+# print(a-100)
+# print(a-5.0)
+
+# print(a*b)
+# print(2*2)
+# print(a*2)
+# print(a*0.2)
+
+
+# print(a/b)
+# print(type(a/b))
+# print(8/2)
+# print(a/250)
+#
+# print(a//b)
+# print(5//2)
+# print(a/150)
+
+# print(a**b)
+# print(5**2)
+# print(a**2)
+#
+# print(a % b)
+# print(5%2)
+# print(a % 150)
+#
+# number = 123
+# print(type(number))
+# some_text = 'some string'
+# number =str(number)
+# print(some_text + number)
+
+# print(str(100) + '' + str(0.2) + '-' +
+# 'Hello world ' + str(True) + '' + a)
+
+# print(type(input()))
+#
+# user_input = input('Please enter some text: ')
+# print (user_input)
+
+# print(len("Grigori"))
+
+# a = float(input("Please enter side A: "))
+# b = float(input("Please enter side B: "))
+# c = float(input("Please enter side C: "))
+
+a, b, c = input('Please enter 3 side: ').split(" ")
+p = (float(a) + float(b) + float(c)) / 2
+print(p)
+# a,b,c= 4,5,6
+
+
+# p = (a + b + c) / 2
+# print(p)
+# # # p - half perimeter
+# S = (p*(p-a)*(p-b)*(p-c)) ** 0.5
+# print(S)
+#
+#
