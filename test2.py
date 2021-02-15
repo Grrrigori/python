46963689cc36eaee9ddf587290577a99366bc206
Grrrigori <gregoryest@gmail.com>
2021-02-15 17:22:11 +0200

    First commit

--- test2.py
+++ test2.py
@@ -1,21 +1,49 @@
-user_input = input('Please insert ID code: ')
-condition3 = (int(user_input[0])*1 + int(user_input[1])*2 + int(user_input[2])*3 + int(user_input[3])*4 + int(user_input[4])*5 + int(user_input[5])*6 + int(user_input[6])*7 + int(user_input[7])*8 + int(user_input[8])*9 + int(user_input[9])*1)%11
-print(condition3)
+# import calendar
+#
+# print(calendar.month(2020, 11, w=4, l=0))
+#
+# # print(calendar.calendar(2020, w=1, l=1, c=15, m=4))
+#
+#
+# print(calendar.TextCalendar.prmonth(theyear= 2021, themonth= 2, w=1, l =1))
+#
+#
+#
+# import time
+# print(time.asctime())
+# print(time.gmtime())
+# print(type(time.gmtime()))
+#
+# now = time.gmtime()
+# print(now[0], now[1], now[2])
+# start = time.time()
+# counter= 0
+# while counter <1000000:
+#     print('Hello')
+#     counter += 1
+# end = time.time()
+# # time.sleep(7)
+#
+# end = time.time()
+#
+# print(end-start)
 
+import datetime
 
-
-
-condition4 = (int(user_input[0]) * 3 + int(user_input[1]) * 4 + int(user_input[2]) * 5 + int(
-                user_input[3]) * 6 + int(user_input[4]) * 7 + int(user_input[5]) * 8 + int(user_input[6]) * 9 + int(
-                user_input[7]) * 1 + int(user_input[8]) * 2 + int(user_input[9]) * 3) % 11
-print(condition4)
-# condition3 = True
-# while condition3:
-#     try:
-#         if condition3 == int(user_input[10]):
-#             print('ID code correct')
-#     except:
-#         print('ID code is not correct')
-#     finally:
-#         print('Happy Ending')
-#         break
+d = datetime.date(2020,7,22)
+print(d)
+today = datetime.date.today()
+print(today)
+some_date = datetime.date(2020,6,12)
+# tdelta = datetime.timedelta(days= 7)
+# print(today + tdelta)
+# delta ne data a raznica vo vremeni
+# date2 = date1 + timadelta
+# timedelta= date1-date2
+# the_day = datetime.date(2007,3,28)
+# time= today -the_day
+# print(time)
+#
+# bday = datetime.date(2021, 5, 4)
+# till_bday= bday-today
+# print(till_bday)
---
+++ venv/test/test1.py
@@ -0,0 +1,58 @@
+# zadanie odin
+
+# customer_input= input("Please enter Your Surname:")
+# customer_input2= input("Please enter Your Name: ")
+# customer_input3= input( "Please enter your age:")
+#
+# print("Hello " + customer_input, customer_input2 + '. Your age is: ' + customer_input3 )
+
+# zadanie dva
+
+# side_a= int(input('enter side a:'))
+# side_b= int(input("enter side b: "))
+#
+# c=((side_a*side_a) +(side_b*side_b))**0.5
+#
+# print(c)
+
+# zadanie tri
+
+# side_a = float(input('enter side a: '))
+# side_b = float(input('enter side b: '))
+# side_c = float(input('enter side c: '))
+#
+# if side_c*side_c == (side_a*side_a + side_b*side_b):
+#     print("The triangle is right triangle")
+# else: print("The triangle is not right triangle")
+
+#  zadanie chetire
+
+# row = [25, 35, 45, 55, 65]
+# print(row[::-1])
+
+#  zadanie pjat
+
+# A = (1,2,3,5,8,)
+# B = (8,2,5,)
+#
+# C= A+B
+# print(C)
+
+#  zadanie 7
+
+# a = ('rotator')
+# # print(len(a))
+# # b = a[0],a[1],a[2],a[3],a[4],a[5],a[6]
+# # print(b)
+# # c = a[-4:-1]
+# # d = a[0:3]
+#
+# if a == a[::-1]:
+#     print("That's a palindrom")
+# else:
+#     print("That's not a palindrom")
+
+#  zadanie 6
+a = [1,2,9,3,4,7,9]
+print(a.count(9))
+
